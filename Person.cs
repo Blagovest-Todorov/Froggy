8bb5243f3b7a9bc5b9b7fc83c59c9b5f1f4a402e
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-13 16:21:26 +0300

    Program.cs

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public struct Person
+    {
+        private string personName;
+        public Person(string name ) 
+        {
+            this.personName = name;
+        }
+
+        public override string ToString()
+        {
+            return this.personName;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace Test
+{
+          
+
+    class IsTest
+    {
+        class Class1 
+        {
+        
+        }
+        class Class2 
+        {
+          
+        }
+        
+        static void Main(string[] args)
+        {
+            object[] myObjects = new object[6];
+            myObjects[0] = new Class1();
+            myObjects[1] = new Class2();
+            myObjects[2] = "string";
+            myObjects[3] = 32;
+            myObjects[4] = null;
+
+            for (int i = 0; i < myObjects.Length; i++)
+            {
+                string s = myObjects[i] as string;
+
+                Console.Write("{0}: ", i);
+
+                if (s != null)
+                {
+                    Console.WriteLine("'" + s + "'");
+                }
+                else
+                {
+                    Console.WriteLine("not a string");
+                }                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
